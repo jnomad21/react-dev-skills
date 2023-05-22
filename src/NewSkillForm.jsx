c104a6fb5c27594abb428c6188e3ab9a30e187a4
Jason McGrath <jnomad21@yahoo.com>
2023-05-22 16:40:03 -0700

    hw 1

---
+++ src/NewSkillForm.jsx
@@ -0,0 +1,19 @@
+export default function NewSkillForm(){
+    return (
+        
+        <form>
+            <label for="input">Skill <input type="text"/>
+            </label>
+            <label for="select">Level
+                <select>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+                <button>ADD SKILL</button>
+            </label>        
+        </form>
+    )
+}
