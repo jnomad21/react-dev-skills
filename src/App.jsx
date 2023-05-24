07904f5373c9d87fb6d37c82d35750eaf4c3adc7
Jason McGrath <jnomad21@yahoo.com>
2023-05-24 15:47:46 -0700

    Completed React Dev Skills lab

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,16 @@ export default function App() {
   { name: "JavaScript", level: 4 },
   { name: "Python", level: 2 },
   ])
+
+  const addSkills = (newSkills)=>
+    setSkills([...skills, newSkills])
+  
   return (
     <div className="App">
       <h1>REACT DEV SKILLS</h1>
       <SkillList skills ={skills}/>
       
-      <NewSkillForm />
+      <NewSkillForm addSkills={addSkills}/>
     </div>
   );
 }
--- src/NewSkillForm.jsx
+++ src/NewSkillForm.jsx
@@ -1,20 +1,39 @@
 import './NewSkillForm.css'
-export default function NewSkillForm(){
+import { useState } from 'react'
+export default function NewSkillForm({addSkills}){
+    const [newSkills, setNewSkills]=useState ({name:'', level:3})
+    
+    function handleChange(e){
+        setNewSkills({
+            ...newSkills,
+            [e.target.name]: e.target.value
+        })
+    }
+    function handleAddSkills(e){
+        e.preventDefault()
+        addSkills(newSkills)
+        setNewSkills({
+            skill: '',
+            level: 3
+        })
+    }
     return (
         
-        <form className="formy">
-            <label for="input">Skill <input type="text"/>
-            </label>
+        <form className="formy" onSubmit={handleAddSkills}>
+            Skill <input type="text" name="name"
+            value={newSkills.name}
+            onChange={handleChange}/>
+
             <br />
             <label for="select">Level
-                <select>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
+                <select name="level" value={newSkills.level} onChange={handleChange}>
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
                 </select>
-                <button>ADD SKILL</button>
+                <button type="submit">ADD SKILL</button>
             </label>        
         </form>
     )
